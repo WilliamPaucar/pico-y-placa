24aa2d7d0c036b8f6c873c3f88eb91706279ce6e
William Alejandro C.m <crazzy.wili1990@hotmail.com>
2024-12-13 10:23:59 -0500

    optimizar logica agregamos el metodo isValid para realizar las validaciones al numero de placa ingresado

--- src/classes/plate.ts
+++ src/classes/plate.ts
@@ -2,21 +2,33 @@
 
 export class Plate {
     private plateNumber: string;
-  
+  // directamente en el constructor comprobamos si el numero de placa es valido 
     constructor(plateNumber: string) {
-      const regex = /^[A-Za-z0-9]{6,7}$/;
-      if (!regex.test(plateNumber)) {
+      this.plateNumber = plateNumber;
+
+      // Si no es válida, lanza un error
+      if (!this.isValid()) {
         throw new Error('Número de placa inválido');
       }
-      this.plateNumber = plateNumber;
     }
+  // metodo obtener el ultimo digito de la placa
+  getLastDigit(): number {
+    return parseInt(this.plateNumber.slice(-1));
+  }
   
-    getLastDigit(): number {
-      return parseInt(this.plateNumber.slice(-1));
-    }
-  
+  // metodo obtener el numero de placa
     getPlateNumber(): string {
       return this.plateNumber;
     }
+      // Método isValid para comprobar si el número de placa tiene un formato válido
+  isValid(): boolean {
+    // Expresión regular para validar placas de 6 a 7 caracteres alfanuméricos
+    // 3 letras seguidas de 3 a 4 números
+    const regex = /^[A-Za-z]{3}[0-9]{4}$/;  // Ejemplo: ABC123 (3 letras y 3 números)
+const regexAlternate = /^[A-Za-z]{2}[0-9]{4}$/;  // Ejemplo: AB1234 (2 letras y 4 números)
+
+    // Retorna true si la placa coincide con uno de los formatos, false si no
+    return regex.test(this.plateNumber) || regexAlternate.test(this.plateNumber);
+  }
   }
   
